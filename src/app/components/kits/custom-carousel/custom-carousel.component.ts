5dfeaeb2b065d3f2a5ff1f874c1a005e0129b68e
faridhash76 <kingfarid76@gmail.com>
2024-05-10 10:16:16 -0700

    fixing the slider in the component

--- src/app/components/kits/custom-carousel/custom-carousel.component.ts
+++ src/app/components/kits/custom-carousel/custom-carousel.component.ts
@@ -3,32 +3,62 @@ import { trigger, transition, style, animate } from '@angular/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomCarouselModule } from './custom-carousel.module';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-custom-carousel',
   standalone: true,
-  imports: [CustomCarouselModule],
+  imports: [CustomCarouselModule, CommonModule],
   animations: [],
 
   templateUrl: './custom-carousel.component.html',
   styleUrl: './custom-carousel.component.css',
 })
 export class CustomCarouselComponent {
-  images = [
-    'https://via.placeholder.com/600x300/FF5733/FFFFFF',
-    'https://via.placeholder.com/600x300/3498DB/FFFFFF',
-    'https://via.placeholder.com/600x300/2ECC71/FFFFFF',
-    'https://via.placeholder.com/600x300/F1C40F/FFFFFF',
+  items = [
+    {
+      image:
+        'https://dkstatics-public.digikala.com/digikala-adservice-banners/7f4b5525cbb171d64f251177948565a7ed68620e_1712133860.jpg?x-oss-process=image/quality,q_95/format,webp',
+      title: 'Slide 1',
+      description: 'Description for slide 1',
+    },
+    {
+      image:
+        'https://dkstatics-public.digikala.com/digikala-adservice-banners/ca571d902c5f361ee39c7734bc05b0f253a6f53b_1714980147.jpg?x-oss-process=image/quality,q_95/format,webp',
+      title: 'Slide 2',
+      description: 'Description for slide 2',
+    },
+    {
+      image:
+        'https://dkstatics-public.digikala.com/digikala-adservice-banners/ebcd2737d616dfa853c97d9b3486282ce315d646_1715268861.jpg?x-oss-process=image/quality,q_95/format,webp',
+      title: 'Slide 3',
+      description: 'Description for slide 3',
+    },
   ];
 
-  currentSlide = 0;
+  activeIndex = 0;
+  timer: any;
 
-  prevSlide() {
-    this.currentSlide =
-      this.currentSlide === 0 ? this.images.length - 1 : this.currentSlide - 1;
+  ngOnInit() {
+    this.startTimer();
   }
 
-  nextSlide() {
-    this.currentSlide =
-      this.currentSlide === this.images.length - 1 ? 0 : this.currentSlide + 1;
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.next();
+    }, 3000); // Set interval time in milliseconds (e.g., 3000ms = 3 seconds)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  next() {
+    this.activeIndex =
+      this.activeIndex === this.items.length - 1 ? 0 : this.activeIndex + 1;
+  }
+
+  prev() {
+    this.activeIndex =
+      this.activeIndex === 0 ? this.items.length - 1 : this.activeIndex - 1;
   }
 }
